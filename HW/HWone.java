ca28f1e25cef2cee206e5bdd9ad6beddefe0375a
nikolay_matev <matev_n@mail.ru>
2022-12-28 14:36:48 +0200

    1st HW

---
+++ HW/HWone.java
@@ -0,0 +1,13 @@
+package HW;
+
+public class HWone {
+    public static void main(String[] args) {
+        for (int p = 0; p < 1000; p++) {
+            int i = (int) (Math.random() * (2000+1)) - 1000;
+
+            System.out.println(i);
+            
+        }
+
+    }
+}
